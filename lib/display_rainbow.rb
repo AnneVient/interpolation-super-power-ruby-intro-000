6703a45e0d7c1d0e16be0d91dc58c3d46534c627
scrappy-system-3850 <aevient@gmail.com>
2019-09-24 19:59:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(colors)
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 puts "R: #{colors [1]}, O: #{colors [2]}, Y: #{colors [3]}, G: #{colors [4]}, B: #{colors [5]}, I: #{colors [6]}, V: #{colors [7]}"
-
+puts display_rainbow(R)
 end
